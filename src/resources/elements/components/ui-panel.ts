afffa6abd49b6d2837339d70344a07fc1dffdd40
Adarsh Pastakia <adarshpastakia@outlook.com>
2016-10-10 17:49:18 +0400

    Added panel-group for making accordions

--- src/resources/elements/components/ui-panel.ts
+++ src/resources/elements/components/ui-panel.ts
@@ -3,28 +3,55 @@
 // @author      : Adarsh Pastakia
 // @copyright   : 2016
 // @license     : MIT
-import {autoinject, bindable, containerless, customElement, inlineView, DOM} from "aurelia-framework";
+import {autoinject, bindable, containerless, customElement, inlineView, children, DOM} from "aurelia-framework";
 import {UIEvent} from "../../utils/ui-event";
+import * as _ from "lodash";
+
+// Panel
+@autoinject()
+@customElement('ui-panel-group')
+@inlineView(`<template class="ui-panel-group" collapse.delegate="__unCollapse()"><slot></slot></template>`)
+export class UIPanelGroup {
+  constructor(public element: Element) { }
+
+  attached() {
+    if (_.find(this.panels, ['collapsed', false]) == null) this.panels[0].collapsed = false;
+  }
+
+  @children('ui-panel') panels;
+
+  __unCollapse() {
+    let panel: any = _.find(this.panels, ['collapsed', false])
+    if (panel) panel.collapsed = true;
+  }
+}
 
 // Panel
 @autoinject()
 @customElement('ui-panel')
-@inlineView(`<template class="ui-panel \${collapsed?'ui-collapse':''}" collapse.trigger="collapsed=!collapsed" close.trigger="closePanel()"><slot></slot></template>`)
+@inlineView(`<template class="ui-panel \${collapsed?'ui-collapse':''}" collapse.trigger="toggleCollapse()" close.trigger="closePanel()"><slot></slot></template>`)
 export class UIPanel {
-  constructor(public element: Element) { }
+  constructor(public element: Element) {
+    this.collapsed = element.hasAttribute('collapsed');
+  }
 
   closePanel() {
     this.element.remove();
+  }
+
+  collapsed = false;
+  toggleCollapse() {
+    setTimeout(() => this.collapsed = !this.collapsed, 200);
   }
 }
 
 @autoinject()
 @customElement('ui-panel-body')
-@inlineView(`<template class="ui-panel-body" css.bind="{'max-height': maxHeight}"><div class="ui-wrapper \${__wrapperClass}"><slot></slot></div></template>`)
+@inlineView(`<template class="ui-panel-body" css.bind="{'max-height': maxHeight}"><slot></slot></template>`)
 export class UIContent {
   constructor(public element: Element) {
     if (element.hasAttribute('scroll')) element.classList.add('ui-scroll');
-    if (element.hasAttribute('padded')) this.__wrapperClass = 'ui-pad-all';
+    if (element.hasAttribute('padded')) element.classList.add('ui-pad-all');
   }
   __wrapperClass = '';
 
@@ -32,40 +59,61 @@ export class UIContent {
 }
 
 @autoinject()
+@customElement('ui-header-tool')
+@inlineView(`<template><button class="ui-header-button ui-\${__type}" click.trigger="fireEvent($event)">
+  <slot><span class="\${__icon}"></span></slot></button></template>`)
+export class UIHeaderTool {
+  constructor(public element: Element) {
+    if (element.hasAttribute('collapse')) this.__type = "collapse";
+    if (element.hasAttribute('collapse')) this.__icon = "fi-ui-angle-up";
+    if (element.hasAttribute('minimize')) this.__type = "minimize";
+    if (element.hasAttribute('minimize')) this.__icon = "fi-ui-dialog-minimize";
+    if (element.hasAttribute('maximize')) this.__type = "maximize";
+    if (element.hasAttribute('maximize')) this.__icon = "fi-ui-dialog-maximize";
+    if (element.hasAttribute('close')) this.__type = "close";
+    if (element.hasAttribute('close')) this.__icon = "fi-ui-cross";
+    if (element.hasAttribute('refresh')) this.__type = "refresh";
+    if (element.hasAttribute('refresh')) this.__icon = "fi-ui-refresh";
+  }
+
+  __type;
+  __icon;
+
+  fireEvent(evt) {
+    if (evt.button != 0) return true;
+    return UIEvent.fireEvent(this.__type, this.element);
+  }
+}
+
+@autoinject()
+@customElement('ui-header-icon')
+@inlineView(`<template class="ui-inline-block"><span class="\${icon}"></span>&nbsp;</template>`)
+export class UIHeaderIcon {
+  constructor(public element: Element) { }
+
+  @bindable() icon = '';
+}
+
+@autoinject()
+@customElement('ui-header-title')
+@inlineView(`<template class="ui-inline-block ui-col-fill"><slot></slot></template>`)
+export class UIHeaderTitle {
+  constructor(public element: Element) { }
+}
+
+@autoinject()
 @customElement('ui-header')
-@inlineView(`<template class="ui-header"><span if.bind="icon" class="\${icon}"></span><slot></slot><span class="ui-col-fill"></span>
-<button class="ui-header-button ui-collapse" if.bind="collapsable" click.trigger="fireEvent($event,'collapse')"><span class="fi-ui-angle-up"></span></button>
-<button class="ui-header-button ui-minimize" if.bind="minimizable" click.trigger="fireEvent($event,'minimize')"><span class="fi-ui-dialog-minimize"></span></button>
-<button class="ui-header-button ui-maximize" if.bind="maximizable" click.trigger="fireEvent($event,'maximize')"><span class="fi-ui-dialog-maximize"></span></button>
-<button class="ui-header-button ui-close" if.bind="closeable" click.trigger="fireEvent($event,'close')"><span class="fi-ui-cross"></span></button>
-</template>`)
+@inlineView(`<template class="ui-header"><slot></slot></template>`)
 export class UIHeader {
   constructor(public element: Element) {
     if (this.element.hasAttribute('primary')) element.classList.add('ui-bg-primary');
     else if (this.element.hasAttribute('secondary')) element.classList.add('ui-bg-secondary');
     else if (this.element.hasAttribute('dark')) element.classList.add('ui-bg-dark');
+    else if (this.element.hasAttribute('light')) element.classList.add('ui-bg-light');
     else if (this.element.hasAttribute('info')) element.classList.add('ui-bg-info');
     else if (this.element.hasAttribute('danger')) element.classList.add('ui-bg-danger');
     else if (this.element.hasAttribute('success')) element.classList.add('ui-bg-success');
     else if (this.element.hasAttribute('warning')) element.classList.add('ui-bg-warning');
-  }
-
-  bind() {
-    this.closeable = isTrue(this.closeable);
-    this.collapsable = isTrue(this.collapsable);
-    this.minimizable = isTrue(this.minimizable);
-    this.maximizable = isTrue(this.maximizable);
-  }
-
-  @bindable() icon = '';
-  @bindable() closeable = false;
-  @bindable() collapsable = false;
-  @bindable() minimizable = false;
-  @bindable() maximizable = false;
-
-  fireEvent(evt, type) {
-    if (evt.button != 0) return true;
-    return UIEvent.fireEvent(type, this.element);
   }
 }
 
